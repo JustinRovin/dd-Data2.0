53c97e5d898874b7f56f1b446a2c43f2cd69076e
AzureEnigma <dmangin@calpoly.edu>
2014-11-05 00:04:44 -0800

    Fixed

--- DB-importData.sql
+++ DB-importData.sql
@@ -4,13 +4,13 @@ INSERT INTO tester.Person(pid, last, first)
 SELECT pid, last, first
 FROM digitaldemocracy.Person;
 
-INSERT INTO tester.Legislator(pid)
-SELECT pid
-FROM digitaldemocracy.Legislator;
-
 INSERT INTO tester.Legislator(description)
 SELECT description
 FROM digitaldemocracy.Person;
+
+INSERT INTO tester.Legislator(pid)
+SELECT pid
+FROM digitaldemocracy.Legislator;
 
 INSERT INTO tester.Term(pid, year, district, house, party, start, end)
 SELECT pid, year, district, house, party, start, end
