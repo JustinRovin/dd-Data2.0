93eda67af06188409ae2271bbb4b0ac601ab8f2d
AzureEnigma <dmangin@calpoly.edu>
2014-11-06 12:54:13 -0800

    Try this

--- DB-importData.sql
+++ DB-importData.sql
@@ -1,12 +1,17 @@
-USE tester;
+USE digitaldemocracy;
 
 INSERT INTO tester.Person(pid, last, first)
 SELECT pid, last, first
 FROM digitaldemocracy.Person;
 
+INSERT INTO tester.Legislator(pid)
+SELECT pid
+FROM digitaldemocracy.Legislator;
+
 INSERT INTO tester.Legislator
 SELECT pid, description
-FROM digitaldemocracy.Person;
+FROM digitaldemocracy.Person JOIN digitaldemocracy.Legislator
+WHERE Legislator.pid = Person.pid;
 
 INSERT INTO tester.Term(pid, year, district, house, party, start, end)
 SELECT pid, year, district, house, party, start, end
